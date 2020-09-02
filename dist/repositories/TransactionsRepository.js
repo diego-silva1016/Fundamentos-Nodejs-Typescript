50425d42d60466b01e60e89cae19e055cf18cc4c
Diego <diego2222110@gmail.com>
2020-09-01 21:32:28 -0300

    Transacoes

---
+++ dist/repositories/TransactionsRepository.js
@@ -0,0 +1,45 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var Transaction_1 = __importDefault(require("../models/Transaction"));
+var TransactionsRepository = /** @class */ (function () {
+    function TransactionsRepository() {
+        this.transactions = [];
+    }
+    TransactionsRepository.prototype.all = function () {
+        return this.transactions;
+    };
+    TransactionsRepository.prototype.getBalance = function () {
+        var income = 0;
+        var outcome = 0;
+        var total = 0;
+        this.transactions.forEach(function (transaction) {
+            if (transaction.type === 'income')
+                income += transaction.value;
+        });
+        this.transactions.forEach(function (transaction) {
+            if (transaction.type === 'outcome')
+                outcome += transaction.value;
+        });
+        total = income - outcome;
+        var balance = {
+            income: income,
+            outcome: outcome,
+            total: total,
+        };
+        return balance;
+    };
+    TransactionsRepository.prototype.create = function (_a) {
+        var title = _a.title, value = _a.value, type = _a.type;
+        var transaction = new Transaction_1.default({ title: title, value: value, type: type });
+        var balance = this.getBalance();
+        if (type === 'outcome' && value > balance.total)
+            throw Error('o valor extrapola o que você possui em sua conta');
+        this.transactions.push(transaction);
+        return transaction;
+    };
+    return TransactionsRepository;
+}());
+exports.default = TransactionsRepository;
--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,9 +1,16 @@
 import Transaction from '../models/Transaction';
+import transactionRouter from '../routes/transaction.routes';
 
 interface Balance {
   income: number;
   outcome: number;
   total: number;
+}
+
+interface TransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
 }
 
 class TransactionsRepository {
@@ -14,15 +21,43 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    let income = 0;
+    let outcome = 0;
+    let total = 0;
+
+    this.transactions.forEach(transaction => {
+      if (transaction.type === 'income') income += transaction.value;
+    });
+    this.transactions.forEach(transaction => {
+      if (transaction.type === 'outcome') outcome += transaction.value;
+    });
+
+    total = income - outcome;
+
+    const balance = {
+      income,
+      outcome,
+      total,
+    };
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: TransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    const balance = this.getBalance();
+
+    if (type === 'outcome' && value > balance.total)
+      throw Error('o valor extrapola o que você possui em sua conta');
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
